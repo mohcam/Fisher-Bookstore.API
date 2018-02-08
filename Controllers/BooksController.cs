d32d8aa301562e9f85692855f7aa44dfd1e4a8aa
mohcam <mohcam@gmail.com>
2018-02-07 19:24:31 -0500

    Created an Api nd a Controller and Model for the Bookstore

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fisher.Bookstore.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller 
+    {
+        private readonly BookstoreContext db;
+        public BooksController(BookstoreContext db){
+            this.db = db;
+            if(this.db.Books.Count()==0){
+                this.db.Books.Add( new Book{
+                    Id = 1,
+                    Title = "The Lean Startup"
+                });
+
+                 this.db.Books.Add( new Book{
+                    Id = 2,
+                    Title = "The Lean Startup"
+                });
+
+                this.db.SaveChanges();
+            }
+            
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok(db.Books);
+        }
+
+    }
+    
+    }
+
