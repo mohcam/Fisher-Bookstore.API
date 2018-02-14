c41ef44ea4a5e9668210d532a9b817968fd6a6f2
mohcam <mohcam@gmail.com>
2018-02-14 18:05:25 -0500

    Created an API for book controller with a GET PUT DELETE POST structure

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -29,12 +29,92 @@ namespace Fisher.Bookstore.Api.Controllers
             
         }
 
+// this is the return method for book call
         [HttpGet]
 
         public IActionResult GetAll()
         {
             return Ok(db.Books);
         }
+
+//Another Get method to get book by its id
+    [HttpGet("{id}",Name="GetBook")]
+   
+    public IActionResult GetByID(int id)
+    {
+        var book = db.Books.Find(id);
+
+        if(book == null)
+        {
+            return NotFound();
+        }
+        return Ok(book);
+    }
+
+    //This is a  a Post method  to post  a book
+    [HttpPost]
+    public IActionResult Post([FromBody]Book book)
+    {
+        //if book returns null, there is a bad request wrning
+        if(book == null)
+        {
+            return BadRequest();
+
+        }
+        //if there is  no bad request, then you can add a new book
+        this.db.Books.Add(book);
+        this.db.SaveChanges();
+
+        return CreatedAtRoute("GetBook", new{IDesignTimeMvcBuilderConfiguration= book.Id}, book);
+    } 
+
+    [HttpPost("id")]
+
+    // this method will insert a book into the database
+    public IActionResult Put (int id, [FromBody]Book newBook)
+    {
+        if(newBook == null || newBook.Id != id)
+        {
+            return BadRequest();
+        }
+        var currentBook = this.db.Books.FirstOrDefault(x => x.Id == id);
+
+        if (currentBook == null)
+        {
+            return NotFound();
+        }
+
+        //set current book to new book
+        currentBook.Author = newBook.Author;
+        currentBook.PublishDate = newBook.PublishDate;
+        currentBook.Publsher= newBook.Publsher;
+
+        //update and save
+        this.db.Books.Update(currentBook);
+        this.db.SaveChanges();
+
+        return NoContent();
+    }
+
+        // this is a delete method to delete a book out of the the db
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var book = this.db.Books.FirstOrDefault(x => x.Id==id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            this.db.Books.Remove(book);
+            this.db.SaveChanges();
+
+            return NoContent();
+
+        }
+
+
 
     }
     
